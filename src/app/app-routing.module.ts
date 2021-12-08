edcd9e995f5b9c9c78d8773e9a5a961493af4efa
Mohit <mohitk@athmin.com>
2021-12-08 12:21:00 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseOverviewComponent } from './routes/course-overview/course-overview.component';
+import { DashboardComponent } from './routes/dashboard/dashboard.component';
+import { ProfileComponent } from './routes/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'overview', component: CourseOverviewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './routes/dashboard/dashboard.component';
+import { ProfileComponent } from './routes/profile/profile.component';
+import { CourseOverviewComponent } from './routes/course-overview/course-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ProfileComponent,
+    CourseOverviewComponent
   ],
   imports: [
     BrowserModule,
